789924eb8b36adba80d9ad71123b3e7065cac38f
Ming <61045393+minying1999@users.noreply.github.com>
2022-05-03 22:57:24 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,26 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  //rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3d6fcf53bdbc4e8484104b7f890cd55f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  
+  name: "Wangting's homework",
+  domain: 'https://unexpected-nerine-6ea.notion.site/Homework-Web-3d6fcf53bdbc4e8484104b7f890cd55f',
+  author: 'Wangting',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: '',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
